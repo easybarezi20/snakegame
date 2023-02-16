b10834af8e36800a0f865d8c54a4a31c1abeb887
easybarezi20 <115116859+easybarezi20@users.noreply.github.com>
2023-02-15 20:38:15 -0800

    detects food collision and scoreboard update

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from turtle import Screen
 from snake import Snake
 from food import Food
+from scoreboard import ScoreBoard
 import time
 
 screen = Screen()
@@ -11,6 +12,7 @@ screen.tracer(0)
 
 snake = Snake()
 food = Food()
+scoreboard = ScoreBoard()
 
 screen.listen()
 screen.onkey(snake.up,"Up")
@@ -26,7 +28,8 @@ while game_is_on:
 
     #detect collision with food
     if snake.head.distance(food) < 15:
-        print("nom nom nom")
+        food.refresh()
+        scoreboard.increase_score()
     
 
 
